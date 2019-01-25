36f64b85dbc4af34a020cc6e0683f1185dd4d17e
srikath <USEER@192.168.0.120>
2019-01-25 13:21:51 +0530

    added method in testproduct

--- src/test/java/com/slokam/opencart/testcases/TestProduct.java
+++ src/test/java/com/slokam/opencart/testcases/TestProduct.java
@@ -1,7 +1,16 @@
 package com.slokam.opencart.testcases;
 
 public class TestProduct {
+	
+	
+	public void product(){
+		System.out.println("hi.......");
+	}
 
 	
-	
+	public static void main(String[] args) {
+		TestProduct pd = new TestProduct();
+		pd.product();
+		
+	}
 }
