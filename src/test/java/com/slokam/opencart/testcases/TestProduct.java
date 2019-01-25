4d2de127217db2d7c9ca945a2775db9d3c8fc14d
srikath <USEER@192.168.0.120>
2019-01-25 16:53:23 +0530

    another method added in testproduct

--- src/test/java/com/slokam/opencart/testcases/TestProduct.java
+++ src/test/java/com/slokam/opencart/testcases/TestProduct.java
@@ -8,12 +8,20 @@ public class TestProduct {
 	}
 	public void product1(){
 		System.out.println("helo.......");
+		
 	}
+	
+	public void product2(){
+		System.out.println("helo2.......");
+		
+	}
+	
 	
 	public static void main(String[] args) {
 		TestProduct pd = new TestProduct();
 		pd.product();
 		pd.product1();
+		pd.product2();
 		
 	}
 }
